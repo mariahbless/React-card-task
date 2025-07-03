8c1c4ee9fa0528f0a382ba586551b4fd7015ffda
Akello mary <akellomary665@gmail.com>
2025-07-03 13:58:57 +0300

    added the picture

--- src/header/head.js
+++ src/header/head.js
@@ -13,13 +13,18 @@ import React from "react";
 // export { Header };
 //import React from "react";
 import myPhoto from '../assets/1000005666.jpg';
- // adjust the path based on your structure
+import mary2 from '../assets/mary2.jpg'
+import './header.css'
 
 function Pic() {
   return (
-    <div>
-      <h1>Welcome to My Garage, Have A seat</h1>
-      <img src={myPhoto} alt="My" width="600" height="450"/>
+    <div className="mi-image">
+      <h1>Welcome to My Garage, Have A seat!!</h1> <b/>
+      <h1>Reception</h1>
+      <img src={myPhoto} alt="My" width="600" height="450"/>General Manager Mary
+      
+      <img src={mary2} alt="May" >
+      </img>
     </div>
   );
 }
